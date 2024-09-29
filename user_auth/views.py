86706573b2039e35a7539054a484f03e3ee96805
Dewansh Rawat <dewanshrawat15@gmail.com>
2024-09-30 01:33:20 +0530

    base exploration

---
+++ user_auth/views.py
@@ -0,0 +1,23 @@
+import json
+
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
+
+from .serialisers import NewUserSerializer
+
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+
+class UserRegistration(APIView):
+    permission_classes = (AllowAny,)
+    serializer_class = NewUserSerializer
+
+    @swagger_auto_schema(request_body=NewUserSerializer)
+    def post(self, request):
+        serializer = NewUserSerializer(data = request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
